294e7954a3172305617eb78bac81df74d1f8f874
Mario Valero <mario102296@hotmail.com>
2026-07-24 16:23:31 -0600

    feat: first commit

---
+++ client/src/data/proyectsData.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Pokedex",
+    description: "Aplicación que permite a usuarios explorar, buscar y gestionar pokémon. Los usuarios pueden crear equipos de hasta 6 miembros, marcar favoritos y compartir su información de forma segura.",
+    tags: ["JavaScript", "Node.js", "Express.js", "Sequelize ORM", "PostgreSQL", "Supabase Auth"],
+    repositorio: ["https://gitlab.com/mariovalero/pokedex-backend/-/tree/main", "https://gitlab.com/MarioVM96/pokedex-frontend"],
+    deploy: "" // Opcional
+  },
+  {
+    id: 2,
+    title: "Portafolio Profesional",
+    description: "Mi sitio web personal diseñado para mostrar mis habilidades como desarrollador y mi galería de proyectos de software.",
+    tags: ["React", "Vite", "Tailwind CSS", "Javascript"],
+    github: "https://github.com/MarioVM96/mariovm-portfolio",
+    deploy: ""
+  },
+  {
+    id: 3,
+    title: "KOFit",
+    description: "Sitio de ventas sobre equipo para boxeadores",
+    tags: ["Nodejs", "Bootstrap", "HTML", "CSS"],
+    github: "https://github.com/MarioVM96/mariovm-portfolio",
+    deploy: ""
+  }
+];
